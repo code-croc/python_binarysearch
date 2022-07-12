9b599bcd8f90e6a5cb4f73c601e98c3c4e94ff03
code-croc <caldwell.vincent@outlook.com>
2022-07-12 10:39:53 -0500

    implements binary search function in main.py and does basic tests it

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+#binary search 
+#searches sequence for value toFind
+#if not found returns -1
+
+import collections
+from msilib import sequence
+
+
+def binarysearch(array , toFind):
+    if not isinstance(array , collections.sequence):
+        return -1
+
+    min = 0
+    max = array.length -1
+    guess = 0
+
+    
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main();
