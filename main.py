33b63ad7a82f4978c4ad82b7ac1144e79015312e
code-croc <caldwell.vincent@outlook.com>
2022-07-12 10:44:04 -0500

    fixes previous commit

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-
+import math
+import string
 
 #binary search 
-#searches sequence for value toFind
+#searches sorted sequence for value toFind
+#if found returns the index
 #if not found returns -1
-
-import collections
-from msilib import sequence
-
-
-def binarysearch(array , toFind):
-    if not isinstance(array , collections.sequence):
+def binarysearch(seq , toFind):
+    if not type(seq) in (tuple, list):
         return -1
 
     min = 0
-    max = array.length -1
+    max = len(seq) -1
     guess = 0
-
+    while max >= min:
+        guess = (min + max) // 2;
+        if seq[guess] == toFind:
+            return guess
+        elif seq[guess] < toFind:
+            min = guess + 1
+        else:
+            max = guess -1
+    
+    return -1
     
 
 def main():
-    return 0
+    print("index: " + str(binarysearch([0,1,2,3,4,5,6,7,8,9], 4)))
+    l = []
+    l.extend(range(0 , 1000));
+    print("index: " + str( binarysearch(l,  1001)))
+    print("index: " + str(binarysearch( (0,1,2,3,4,5,6,7,8,9), 2)))
+    print("index: " + str(binarysearch('A', 4)))
+    print("index: " + str(binarysearch("ABCDEFG", 'E')))
 
 if __name__ == '__main__':
-    main();
+    main()
